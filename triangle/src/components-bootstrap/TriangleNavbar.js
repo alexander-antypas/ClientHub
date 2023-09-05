d0d3c0a46102ba7abbf333dbc455035039de9414
alekos antypas <alexander.ant.98@gmail.com>
2023-09-05 14:45:02 +0300

    pages navbar complete

--- triangle/src/components-bootstrap/TriangleNavbar.js
+++ triangle/src/components-bootstrap/TriangleNavbar.js
@@ -1,29 +1,49 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import SimplifiedMethod from './SimplifiedMethod';
+import AnalyticalMethod from './AnalyticalMethod';
+import Home from './Home';
+import About from './About';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
+import Mulesme from './Mulesme';
 function TriangleNavbar() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-        <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/triangle_image.jpg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{'   '}
-            Triangle
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Tools</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Router>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+          <Navbar.Brand>
+              <img
+                alt=""
+                src="/triangle_image.jpg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{'  '}
+              Triangle
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/home">Home</Nav.Link>
+              <NavDropdown title="Tools" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/simplified">Simplified Triangle Method</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/analytical">Analytical Triangle Method</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to="/mulesme">Mulesme Downscale Method</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path="/home" element={<Home/>}></Route>
+          <Route path="/simplified" element={<SimplifiedMethod />}></Route>
+          <Route path="/analytical" element={<AnalyticalMethod />}></Route>
+          <Route path="/mulesme" element={<Mulesme />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
